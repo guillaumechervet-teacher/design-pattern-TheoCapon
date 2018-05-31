791eb98c957b03ba9f8f116f45a3476418b687a3
Theo Capon <t.capon@acticall.com>
2018-05-31 12:11:48 +0200

    suite

---
+++ src/Basket/Basket/ImperativeProgramming.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Basket
+{
+    public class ImperativeProgramming
+    {
+        public static int CalculateBasketAmount(IList<BasketLineArticle> basketLineArticles) 
+        { 
+// here your code implementation
+            
+        }
+        
+        public static ArticleDatabase GetArticleDatabase(string id) 
+        { 
+// here your code implementation
+        }
+    }
+}
