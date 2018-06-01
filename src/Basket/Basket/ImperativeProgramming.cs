ca8bccee0bc65d2a1b8fbf6b11f7d89a97596aa4
Theo Capon <t.capon@acticall.com>
2018-06-01 17:24:13 +0200

    suite

--- src/Basket/Basket/ImperativeProgramming.cs
+++ src/Basket/Basket/ImperativeProgramming.cs
@@ -48,5 +48,27 @@ namespace Basket
                var article = articleDatabases.First(articleDatabase => articleDatabase.Id == id);
                return article;
            }
+       
+       public static ArticleDatabase GetArticleDatabaseMock(string id)
+       { 
+           switch(id) 
+           { 
+               case "1": 
+                   return new ArticleDatabase {Id = "1", Price = 1, Stock = 35, Label = "Banana", Category = "food"}; 
+               case "2": 
+                   return new ArticleDatabase 
+               { 
+                   Id = "2", 
+                   Price = 500, 
+                   Stock = 20, 
+                   Label = "Fridge electrolux", 
+                   Category = "electronic"
+               }; 
+               case "3": 
+                   return new ArticleDatabase {Id = "3", Price = 49, Stock = 68, Label = "Chair", Category = "desktop"}; 
+               default: 
+                   throw new NotImplementedException(); 
+           } 
+       }
    }
 }
